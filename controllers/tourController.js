75a18d11a2b43e71722778afb4e5e46797008e53
successJohn <successojohn@gmail.com>
2022-01-12 08:59:20 -0800

    feat: get all tours and a single tour endpoint

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,7 @@ const Tour = require("./../models/tourModel");
 exports.createTour = async (req, res) =>{
     try{
         const newTour = await Tour.create(req.body);
+        console.log(newTour);
 
         res.status(201).json({
             status: "success",
@@ -12,6 +13,43 @@ exports.createTour = async (req, res) =>{
         })
     }catch(err){
         res.status(400).json({
+            status: "fail",
+            msg: err
+        })
+    }
+}
+
+exports.getAllTours = async(req, res) =>{
+    try{
+        const allTours = Tour.find();
+        console.log(allTours);
+        res.status(201).json({
+            status: "success",
+           
+            data: {
+                tour: allTours
+            }
+        })
+    }catch(err){
+        res.status(400).json({
+            status: "fail",
+            msg: err
+        })
+    }
+}
+
+
+exports.getTour = async (req, res)=>{
+ try {
+        const tour =  await Tour.findById(req.params.id);
+        res.status(201).json({
+            status: "success",
+            data:{
+                tour
+            }
+        })
+    } catch (error) {
+            res.status(400).json({
             status: "fail",
             msg: err
         })
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,11 @@ const tourController = require("./../controllers/tourController");
 const router = express.Router();
 
 router.route("/")
+.get(tourController.getAllTours)
 .post(tourController.createTour);
+
+
+router.route("/:id")
+.get(tourController.getTour);
 
 module.exports = router;
