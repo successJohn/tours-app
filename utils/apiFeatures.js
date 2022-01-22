dbe8aecbc210e3ff0bc1148528d847772bbfeac1
Success John <successojohn@gmail.com>
2022-01-22 16:36:14 +0100

    feat: added README

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -18,8 +18,7 @@ class APIFeatures{
          queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
  
         this.query = this.query.find(JSON.parse(queryStr));
-         //let data = Tour.find(JSON.parse(queryStr));
-
+        
          console.log(this);
          return this;
          
